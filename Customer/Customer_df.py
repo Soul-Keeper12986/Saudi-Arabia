7763c4430b0bab017d1387911b3b950ff7aa58ac
Nikolay Ladykin <ladykinnikolay@gmail.com>
2022-03-05 18:41:38 +0300

    Add Email, autopay and other

--- Customer/Customer_df.py
+++ Customer/Customer_df.py
@@ -31,22 +31,23 @@ class Customer:
 		self.language = language
 		self.termination_date = termination_date
 
-'''
+
 def generate_First_name(population, gender):
-	if gender == 'Male':
-		with open('Male_Arabic_name_list.csv', newline='') as f:
-			reader = csv.reader(f)
-			data = list(reader)[0]
-	else:
-		with open('Female_Arabic_name_list.csv', newline='') as f:
-			reader = csv.reader(f)
-			data = list(reader)[0]
+	with open('Male_Arabic_name_list.csv', newline='') as file_male:
+		reader = csv.reader(file_male)
+		male_names = list(reader)[0]
+	with open('Female_Arabic_name_list.csv', newline='') as  file_female:
+		reader = csv.reader(file_female)
+		female_names = list(reader)[0]
 	Customer_Names = []
 	for i in range(0, population):
-		Customer_Names.append(np.random.choice(data))
+		if gender[i] == 'Male':
+			Customer_Names.append(np.random.choice(male_names))
+		else:
+			Customer_Names.append(np.random.choice(female_names))
 		#print(Names)
 	return(Customer_Names)
-'''
+
 
 def generate_Date_of_Birth(population):
 	'''
@@ -77,8 +78,26 @@ def generate_Date_of_Birth(population):
 	'''
 	Customer_dates_of_birth = []
 	for i in range(0, population):
-		a = np.random.choice(['0-4','5-9','10-14','15-19','20-24','25-29','30-34','35-39',
-			'40-44','45-49','50-54','55-59','60-64','65-69','70-74','75-79','80-84','85-89'], 
+		a = np.random.choice([
+			'0-4',
+			'5-9',
+			'10-14',
+			'15-19',
+			'20-24',
+			'25-29',
+			'30-34',
+			'35-39',
+			'40-44',
+			'45-49',
+			'50-54',
+			'55-59',
+			'60-64',
+			'65-69',
+			'70-74',
+			'75-79',
+			'80-84',
+			'85-89'
+			], 
 			p = [0.0816, 0.0853, 0.0817, 0.0728, 0.0693, 0.0746, 0.0959, 0.0941, 0.0888,
 			 0.0853, 0.0639, 0.0444, 0.0302, 0.0178, 0.0071, 0.0036, 0.0018, 0.0018]).split(sep ='-')
 		#print(a)
@@ -92,16 +111,6 @@ def generate_Date_of_Birth(population):
 		Customer_dates_of_birth.append(date_of_birth)
 	return(Customer_dates_of_birth)
 
-    
-def generate_First_name(population):
-	with open('Male_Arabic_name_list.csv', newline='') as f:
-		reader = csv.reader(f)
-		data = list(reader)[0]
-	Customer_Names = []
-	for i in range(0, population):
-		Customer_Names.append(np.random.choice(data))
-		#print(Names)
-	return(Customer_Names)
 
 def generate_Last_name(population):
 	with open('Arabic_Surnames_list.csv', newline='') as f:
@@ -112,14 +121,34 @@ def generate_Last_name(population):
 			Customer_Surnames.append(np.random.choice(data))
 	return(Customer_Surnames)
 
+
 def generate_Region(population):
-	Regions = ['Mecca Region','Riyadh Region','Eastern Region','Asir Region',
-	'Jizan Region','Medina Region','Al-Qassim Region','Hail Region',
-	'Najran Region','Al-Jawf Region','Al-Bahah Region','Northern Borders Region']
+	Regions = {
+				'Mecca':8_099_473,
+				'Riyadh':7_910_864,
+				'Eastern':4_762_871,
+				'Asir':2_194_463,
+				'Medina':2_061_383,
+				'Jizan':1_568_727,
+				'Al-Qassim':1_402_974,
+				'Tabuk':907_494,
+				'Hail':685_820,
+				'Najran':581_789,
+				'Al-Jawf':506_372,
+				'Al-Bahah':471_755,
+				'Northern Borders':367_433
+				}
 	Customer_regions = []
+	probabilities = []
+	sum_population = sum(Regions.values())
+
+	for value in Regions.values():
+		probabilities.append(value/sum_population)
+
 	for i in range(0, population):
-		Customer_regions.append(np.random.choice(Regions))
+		Customer_regions.append(np.random.choice((list(Regions.keys())), p = probabilities))
 	return(Customer_regions)
+
 
 def generate_ID(population):
 	customer_ID_list = np.arange(population)
@@ -127,17 +156,13 @@ def generate_ID(population):
 	#print(customer_ID_list)
 	return(customer_ID_list)
 
-def generate_Gender(population):
-	Genders = []
-	for i in range(population):
-		Genders.append(np.random.choice(['Male', 'Female'], p = [0.539, 0.461]))
-	return(Genders)
 
 def generate_Gender(population):
 	Genders = []
 	for i in range(population):
 		Genders.append(np.random.choice(['Male', 'Female'], p = [0.539, 0.461]))
 	return(Genders)
+
 
 def generate_Language(population):
 	Languages = []
@@ -145,15 +170,61 @@ def generate_Language(population):
 		Languages.append(np.random.choice(['Arabic','English','Other'], p = [0.979, 0.012 ,0.009]))
 	return(Languages)
 
+
 def generate_MSISDN(population):
 	Customer_Numbers = []
+	country_code = "+996"
+	NDC = ['50', '53', '54', '55', '56', '58', '59']
 	for i in range(population):
-		country_code = "+996"
-		NDC = ['50', '53', '54', '55', '56', '58', '59']
 		num =np.random.randint(9999999)
 		number = country_code +'-'+ np.random.choice(NDC)+'-'+(f'{num:07}')
 		Customer_Numbers.append(number)
 	return(Customer_Numbers)
+
+
+def generate_agree_for_promo(population):
+	Customers_agree = []
+	for i in range(population):
+		Customers_agree.append(np.random.choice(['Yes', 'No'], p = [0.4, 0.6]))
+	return(Customers_agree)
+
+
+def generate_Agree_for_promo(population):
+	Customers_agree = []
+	for i in range(population):
+		Customers_agree.append(np.random.choice(['Yes', 'No'], p = [0.5, 0.5]))
+	return(Customers_agree)
+
+
+def generate_Autopay_card(population):
+	Customers_Autopay = []
+	for i in range(population):
+		k = np.random.randint(10,20)/100
+		Customers_Autopay.append(np.random.choice(['Yes', 'No'], p = [0.2+k, 0.8-k]))
+	return(Customers_Autopay)
+
+
+def generate_Email(population, names, surnames):
+	domains = [
+				'awalnet.sa', 
+				'aramco.com.sa', 
+				'saudia-omline.com',
+				'gmail.com', 
+				'naseej.com.sa',
+				'123arab.com',
+				'sol.net.sa',
+				'zajil.net.sa',
+				'sahara.com.sa',
+				'compuserve.com',
+				'sps.net.sa',
+				'arasco.net.sa'
+				]
+	Customers_Emails = []
+	for i in range(population):
+		address =str(names[i]+surnames[i]+str(np.random.randint(100))+'@'+np.random.choice(domains))
+		Customers_Emails.append(address) 
+	return(Customers_Emails)
+
 
 '''
 customer_columns  = ['Customer_ID',
@@ -172,18 +243,24 @@ customer_columns  = ['Customer_ID',
 					'Language',
 					'Termination_date']
 '''
+
 population = 50
 #customer_dataframe = pd.DataFrame(columns = customer_columns)
-customer_dataframe = pd.DataFrame()
+customer_df = pd.DataFrame()
 
-customer_dataframe['Customer_ID'] = generate_ID(population) 
-customer_dataframe['Gender'] = generate_Gender(population) 
-customer_dataframe['First name'] = generate_First_name(population)
-customer_dataframe['Last name'] = generate_Last_name(population) 
-customer_dataframe['Region'] = generate_Region(population) 
-customer_dataframe['Language'] = generate_Language(population) 
-customer_dataframe['Date_of_birth'] = generate_Date_of_Birth(population) 
-customer_dataframe['MSISDN'] = generate_MSISDN(population)
+customer_df['Customer_ID'] = generate_ID(population)
+customer_df['Gender'] = generate_Gender(population) 
+customer_df['First name'] = generate_First_name(population,customer_df['Gender'])
+customer_df['Last name'] = generate_Last_name(population) 
+customer_df['Date_of_birth'] = generate_Date_of_Birth(population) 
+customer_df['Agree_for_promo'] = generate_Agree_for_promo(population)
+customer_df['autoplay_card'] = generate_Autopay_card(population)
+customer_df['Customer_Email'] = generate_Email(population, customer_df['First name'], customer_df['Last name'])
+customer_df['MSISDN'] = generate_MSISDN(population)
+
+customer_df['Region'] = generate_Region(population) 
+customer_df['Language'] = generate_Language(population) 
+
 
 pd.set_option('display.max_columns', None)
 #pd.set_option('display.max_rows', None)
@@ -209,6 +286,5 @@ for i in range(0, population):
 				index = customer_columns),
 		ignore_index = True)
 '''
-print(customer_dataframe)
-
-#customer_dataframe.to_csv('Customers.csv', sep=',')
+print(customer_df)
+customer_df.to_csv('Customers.csv', sep=',')
